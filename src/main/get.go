382a8f63f83006d389d8336ac9fedca539fb3077
Gauthier Voron <gauthier.voron@sydney.edu.au>
2019-01-14 12:17:03 +1100

    doc(get): change the help message of get subcommand
    Change the PrintGetUsage() function so now it displays the new syntax
    for the "get" subcommand.

--- src/main/get.go
+++ src/main/get.go
@@ -10,20 +10,93 @@ var DEFAULT_UPDATE bool = false
 var optionUpdate *bool
 
 func PrintGetUsage() {
-	fmt.Printf(`Usage: %s [options] get fleets
-       %s [options] get instances [<fleet-names...>]
-       %s [options] get ip [<instance-ids...>]
-       %s [options] get user [<instance-ids...>]
+	fmt.Printf(`Usage: %s get [options] fleets
+       %s get [options] <property> <instances-specification...>
 
 Print information about fleets and instances.
-If a fleet name or instance id is provided, then print information about these
-fleets or instances, otherwise, print about every fleets or instances.
+The first form print a list of the fleet names launched and not yet stopped in
+the current context.
+The second form print the value of a property for a list of instances. The
+instances may be specified by their name or by the name of their fleet, either
+with a string or a regular expression.
+If no specification is given, print properties for all instances.
+
+Properties:
+  fleet             name of the fleet of the instances
+  fiid              integer that identifies the instance inside its fleet
+  ip | public-ip    public IPv4 to access the instance
+  name              name of an instance, as defined by AWS
+  private-ip        private IPv4: how the instance sees itself
+  region            region code the instance runs in (e.g. 'us-east-2')
+  uiid              integer that identifies the instance inside its context
+  user              username to use for an ssh connection
+
+Instance specification:
+  Instances can be specified either directly by their name or by the name of
+  their fleet. In this last case, the specification starts with a '@':
+
+      i-0efc03c42c4137c64     the instance identified as 'i-0ef...' by AWS
+
+      @my-fleet               all instances belonging to the fleet 'my-fleet'
+
+  Additionally, an instance or a fleet name can be an exact string or a Perl
+  regular expression. In this last case, it is surrounded by '/':
+
+      /^.*[0-7]$/             all instances with a name ending with 0, ..., 7
+
+      @/fleet-(a|b)/          all instances of fleets 'fleet-a' or 'fleet-b'
+
+  Without additional options, the instances resulting from a single
+  specification are sorted by their uiid property.
+  The results from different specifications are concatenated without additional
+  sorting nor re;oving of duplicate results.
 
 Options:
   --context <path>            path of the context file (default: '%s')
+
+  --sort                      sort instances by their uiid before to print
+                              their properties (shortcut for '--sort-by uiid')
+
+  --sort-by <property>        sort instances by the indicated property before
+                              to print their properties
+
+  --unique-instances          remove duplicate instances before to print their
+                              properties, preserving order (duplicate results
+                              may be displayed if several instances have the
+                              same properties)
+
+  --unique-results            remove duplicate results, preserving order (no
+                              duplicate can be displayed)
+
   --update                    update context before to print
+
+Examples:
+  Print all available fleets, one per line:
+
+      %s get fleets
+      %s get --unique-results fleet
+
+  Print all instance names for fleets 'my-fleet', 'fleet-0' and 'fleet-1':
+
+      %s get name @my-fleet @/^fleet-[01]$/
+
+  Print all public IP addresses for this context:
+
+      %s get public-ip
+      %s get public-ip /^.*$/
+      %s get public-ip //
+
+  Print the maximum id of an instance inside of its fleet:
+
+      %s get --sort-by=fiid --unique-results fiid | tail -n 1
+
+  Print public IP addresses from 'fleet-a' and 'my-fleet' in consisent order:
+
+      %s get --sort ip @my-fleet @fleet-a
 `,
-		PROGNAME, PROGNAME, PROGNAME, PROGNAME, DEFAULT_CONTEXT)
+		PROGNAME, PROGNAME, DEFAULT_CONTEXT,
+		PROGNAME, PROGNAME, PROGNAME, PROGNAME, PROGNAME, PROGNAME,
+		PROGNAME, PROGNAME)
 }
 
 func getFleets(args []string, ctx *Context) {
