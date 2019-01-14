3e6ae72c9b24298b70a2be8774df7d1c92d6107d
Gauthier Voron <gauthier.voron@sydney.edu.au>
2019-01-14 15:41:29 +1100

    style(main): fix parenthesis and space style issues
    Fix some style issues about useless parenthesis and useless spaces
    reported by the gofmt tool.

--- src/main/main.go
+++ src/main/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 var PROGNAME string = "ec2tools"
-var VERSION string  = "1.0.0"
-var AUTHOR string   = "Gauthier Voron"
-var MAILTO string   = "gauthier.voron@sydney.edu.au"
+var VERSION string = "1.0.0"
+var AUTHOR string = "Gauthier Voron"
+var MAILTO string = "gauthier.voron@sydney.edu.au"
 
 func Error(format string, a ...interface{}) {
 	Warning(format, a...)
-	fmt.Fprintf(os.Stderr, "Please type '%s --help' for more " +
+	fmt.Fprintf(os.Stderr, "Please type '%s --help' for more "+
 		"information\n", PROGNAME)
 	os.Exit(1)
 }
@@ -47,7 +47,7 @@ func printVersion() {
 	fmt.Println(AUTHOR)
 	fmt.Println(MAILTO)
 }
-	
+
 func main() {
 	var help *bool = flag.Bool("help", false, "")
 	var version *bool = flag.Bool("version", false, "")
@@ -55,35 +55,35 @@ func main() {
 
 	flag.Parse()
 
-	if (*help) {
+	if *help {
 		PrintUsage()
 		return
 	}
 
-	if (*version) {
+	if *version {
 		printVersion()
 		return
 	}
 
-	if (len(flag.Args()) == 0) {
+	if len(flag.Args()) == 0 {
 		Error("missing command operand")
 	}
 
 	command = flag.Args()[0]
 
-	if (command == "get") {
+	if command == "get" {
 		Get(flag.Args())
-	} else if (command == "help") {
+	} else if command == "help" {
 		Help(flag.Args())
-	} else if (command == "launch") {
+	} else if command == "launch" {
 		Launch(flag.Args())
-	} else if (command == "scp") {
+	} else if command == "scp" {
 		Scp(flag.Args())
-	} else if (command == "ssh") {
+	} else if command == "ssh" {
 		Ssh(flag.Args())
-	} else if (command == "stop") {
+	} else if command == "stop" {
 		Stop(flag.Args())
-	} else if (command == "update") {
+	} else if command == "update" {
 		Update(flag.Args())
 	} else {
 		Error("invalid command operand: %s", command)
