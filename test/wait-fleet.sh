617c566d0a7084880818870ed9ff78d2d0edfcea
Gauthier Voron <gauthier.voron@sydney.edu.au>
2019-01-15 12:05:27 +1100

    test(wait): add a test for the selective 'wait' use case
    Add a test for the 'wait' subcommand when waiting only some fleets.

---
+++ test/wait-fleet.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# Should success
+ec2tools launch --replace --size=1 --price="$TEST_PRICE" --key="$TEST_KEY" \
+	 'test-fleet'
+
+# Should not launch (price too low) and hang for a long time
+ec2tools launch --replace --size=3 --price=0.001 --key="$TEST_KEY" \
+	 'never-fleet'
+
+# Wait only the successful fleet
+ec2tools wait 'test-fleet'
+
+# Check the waited fleet has an ip (1 instance)
+test $(ec2tools get --update ip | wc -l) -eq 1
+ret=$?
+
+ec2tools stop
+
+exit $ret
