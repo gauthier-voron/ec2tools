4f0bd4719a1c61e33c5311f9a978b3e9426ebd78
Gauthier Voron <gauthier.voron@sydney.edu.au>
2019-01-18 13:28:34 +1100

    test(ssh): use the '--wait-for=ssh' option of 'wait'
    Change tests scripts so they use the '--wait-for=ssh' option of the
    'wait' subcommand instead of a manual loop.

--- test/ssh-stderr.sh
+++ test/ssh-stderr.sh
@@ -5,12 +5,7 @@ set -e
 # Launch a fleet of 1 instance and wait for it
 ec2tools launch --replace --size=1 --price="$TEST_PRICE" --key="$TEST_KEY" \
 	 'test-fleet'
-ec2tools wait
-
-# Replace by `ec2tools wait --wait-for=ssh` when available
-while ! ec2tools ssh true ; do
-    sleep 1
-done
+ec2tools wait --wait-for='ssh'
 
 # Create a script that print interleaved output and error before to exit
 script=$(mktemp --suffix='.sh' 'test-script.XXXXXXXXXX')
