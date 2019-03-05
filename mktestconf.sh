e7fda0fe5f7283dc2eee74d8a9218b6be5a98d2e
Gauthier Voron <gauthier.voron@sydney.edu.au>
2019-03-05 12:09:13 +1100

    chore(test): add the TEST_IMAGE test parameter
    Add a parameter TEST_IMAGE to be used in validation tests. It defines
    the prefix of the names used in image creations and copies.

--- mktestconf.sh
+++ mktestconf.sh
@@ -196,13 +196,14 @@ print_header
 
 ask "Ssh key to use for AWS instances" TEST_KEY
 ask "Price to use for AWS instances (USD)" TEST_PRICE
+ask "Name to use for created AWS images" TEST_IMAGE
 
 
 # Step 3: Write the configuration on disk
 # Dump all the assigned variables to the config file.
 #
 
-dump_config TEST_KEY TEST_PRICE > "${CONFIG_PATH}"
+dump_config TEST_KEY TEST_PRICE TEST_IMAGE > "${CONFIG_PATH}"
 print_footer
 
 exit 0
